101ce1097b3a1e29cf15bfedda243fa6a8b22e3a
korut94 <andreamnt94@gmail.com>
2016-12-20 11:29:56 +0100

    Empty class world

--- include/world.h
+++ include/world.h
@@ -2,43 +2,19 @@
 #define WORLD_H
 
 #include <QGraphicsScene>
-#include <QGraphicsView>
 
 /**
  * @brief The environment where anything happens. The class contains all data
- * relatead to the rendering, phisics, logic and the comunication with the
- * Game class.
+ * relatead to the rendering, phisics, logic.
  */
-class World
+class World : public QGraphicsScene
 {
   public:
     /**
      * @brief Default costructor.
-     * @param The parent widget where the scene will be showed.
+     * @param The parent object.
      */
-    explicit World(QWidget *parent = 0);
-    /**
-     * @brief Draw the scene with the up-to-date elements.
-     * @param Percentage distance from the previous tick and the time
-     * when the call to draw it's done.
-     */
-    void draw(const qreal dt);
-    /**
-     * @brief Advance with the logic game of a precise time step.
-     * @param Time step
-     */
-    void update(const qreal elapsedTime);
-
-  private:
-    /**
-     * @brief The world's scene
-     */
-    QGraphicsScene  mScene;
-    /**
-     * @brief The main view. Whatever is inside to the virtual rect drawed by
-     * the view will be showed in the game's window.
-     */
-    QGraphicsView   mView;
+    explicit World(QObject *parent = nullptr);
 };
 
 #endif // WORLD_H
--- source/world.cpp
+++ source/world.cpp
@@ -1,25 +1,4 @@
 #include "world.h"
 
-World::World(QWidget *parent) : mView(&mScene, parent)
-{
-  // Configure the view and the scene's dimension so to take up the whole
-  // parent's graphic space.
-  mScene.setSceneRect(parent->geometry());
-  mView.setSceneRect(parent->geometry());
-
-  // Show the scene on the screen
-  mView.show();
-}
-
-void World::update(const qreal elapsedTime)
-{
-  Q_UNUSED(elapsedTime);
-  mScene.advance();
-}
-
-void World::draw(const qreal dt)
-{
-  Q_UNUSED(dt);
-  // It's no necessary call update to render the last modifies
-  // mScene.update();
-}
+World::World(QObject *parent)
+  : QGraphicsScene(parent) {}
