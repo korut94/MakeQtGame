9ccdbd59d3b2478feffd03716647ce150e04af3a
korut94 <andreamnt94@gmail.com>
2017-02-13 23:07:36 +0100

    Added new interface for containers

---
+++ core/containerinterface.h
@@ -0,0 +1,21 @@
+#ifndef CONTAINERINTERFACE_H
+#define CONTAINERINTERFACE_H
+
+#include "logic/container/containerbook.h"
+
+namespace mqg
+{
+namespace libs
+{
+class ContainerInterface
+{
+public:
+  virtual void saveIn(Logic::Container::ContainerBook &book) const = 0;
+};
+} // namespace libs
+} // namespace mqg
+
+#define ContainerInterface_iid "mqg.libs.ContainerInterface"
+Q_DECLARE_INTERFACE(mqg::libs::ContainerInterface, ContainerInterface_iid)
+
+#endif // CONTAINERINTERFACE_H
