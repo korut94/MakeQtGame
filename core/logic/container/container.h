d4c8306018b98902bb0bb8ea2a70ff6857e089f9
korut94 <andreamnt94@gmail.com>
2017-02-13 23:39:59 +0100

    Container is an aka for a std::vector

--- core/logic/container/container.h
+++ core/logic/container/container.h
@@ -1,50 +1,17 @@
 #ifndef CONTAINER_H
 #define CONTAINER_H
 
-#include <array>
-#include <cstddef>
 #include <vector>
+
+#include "../abstractlogic.h"
 
 namespace mqg
 {
 namespace Logic
 {
-class Logic;
-
-namespace Cotainer
+namespace Container
 {
-class Builder;
-
-template <std::size_t N>
-class Container
-{
-  friend class Builder;
-
-public:
-  void start();
-
-private:
-  Container(const std::vector<Logic*> &buffer);
-
-  std::array<Logic*, N> m_buffer;
-};
-
-template <std::size_t N>
-Container<N>::Container(const std::vector<Logic*> &buffer)
-{
-  std::copy(buffer.begin(), buffer.end(), m_buffer);
-}
-
-template <std::size_t N>
-void Container<N>::start()
-{
-  Logic *current = m_buffer[0];
-
-  while (current != nullptr) {
-    current.run();
-    current = current.getNext();
-  }
-}
+using Container = std::vector<AbstractLogic*>;
 } // namespace Container
 } // namespace Logic
 } // namespace mqg
