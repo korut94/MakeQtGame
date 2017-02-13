267c9853e98e78243b9cf82696ae6da03282e63b
korut94 <andreamnt94@gmail.com>
2017-02-13 23:13:12 +0100

    Include for all loader container module

---
+++ core/logic/container/loader.h
@@ -0,0 +1,9 @@
+#ifndef LOADER_H
+#define LOADER_H
+
+#include "loader/abstractcontainerloader.h"
+#include "loader/containerloader.h"
+#include "loader/envproxycontainerloader.h"
+#include "loader/fcontainerloader.h"
+
+#endif // LOADER_H
