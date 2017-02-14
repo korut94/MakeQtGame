bbdd99ee062a9ca7144ab57a100533391eb43435
korut94 <andreamnt94@gmail.com>
2017-02-14 11:46:43 +0100

    Removed inheritance with QObject and add create method

--- core/entitybook.cpp
+++ core/entitybook.cpp
@@ -4,54 +4,16 @@ namespace mqg
 {
 namespace Entity
 {
-QGraphicsItem* EntityBook::create(const QString &entity)
+QGraphicsItem* EntityBook::create(const QString &entity) const
 {
-  QMap<QString, Factory>::const_iterator itr = m_registerFactory.find(entity);
+  auto itr = m_registerFactory.find(entity);
 
-  if (itr != m_registerFactory.end()) {
-    QGraphicsItem *pEntity = (itr.value())();
-
-    emit entityCreated(pEntity);
-    return pEntity;
-  } else {
-    return nullptr;
-  }
+  return (itr != m_registerFactory.end()) ? (itr.value())() : nullptr;
 }
 
 QStringList EntityBook::list() const
 {
   return m_registerFactory.keys();
-}
-
-QScriptValue EntityBook::wrapperCreate(QScriptContext *context,
-                                       QScriptEngine *engine)
-{
-  QScriptValue object = context->thisObject();
-  EntityBook *book = qobject_cast<EntityBook*>(object.toQObject());
-
-  if (book != nullptr && context->argumentCount() > 0) {
-    // Take only the first argument, the other ones are ignored.
-    QScriptValue arg = context->argument(0);
-
-    if (arg.isString()) {
-      book->create(arg.toString());
-    }
-  }
-
-  return engine->undefinedValue();
-}
-
-QScriptValue EntityBook::wrapperList(QScriptContext *context,
-                                     QScriptEngine *engine)
-{
-  QScriptValue object = context->thisObject();
-  EntityBook *book = qobject_cast<EntityBook*>(object.toQObject());
-
-  if (book == nullptr) {
-    return engine->undefinedValue();
-  } else {
-    return book->list().join(" ");
-  }
 }
 } // namespace Entity
 } // namespace mqg
