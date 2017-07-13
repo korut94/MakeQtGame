0b830797c5ad2aaaf259ea408f10315b1e1eeb59
korut94 <andreamnt94@gmail.com>
2017-07-13 20:06:46 +0200

    Register Engine and Environment as QML item inside the mqg.Core namespace

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    qmlRegisterType<mqg::Engine>("mqg.Core", 1, 0, "Engine");
+    qmlRegisterType<mqg::Core::Engine>("mqg.Core", 1,0, "Engine");
+    qmlRegisterType<mqg::Core::Environment>("mqg.Core", 1,0, "Environment");
 
     QQmlApplicationEngine engine;
     engine.load(QStringLiteral("qrc:/qt-project.org/imports/mqg/main.qml"));
