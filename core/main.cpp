150d902c52fe686253839939c7a425475e990c6b
korut94 <andreamnt94@gmail.com>
2017-07-25 13:18:48 +0200

    Registrazion of Application class

--- core/main.cpp
+++ core/main.cpp
@@ -1,18 +1,39 @@
+#include "application.h"
+#include "jsengineadapterforqt.h"
+#include "messageadapterforqt.h"
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-#include "engine.h"
+#include <QDebug>
+
+using namespace mqg::Core;
+
+static QObject* app_provider(QQmlEngine *engine, QJSEngine *scriptEngine) {
+  Q_UNUSED(engine);
+
+  JSEngineAdapterForQt *script = new JSEngineAdapterForQt(scriptEngine);
+  MessageAdapterForQt *message = new MessageAdapterForQt();
+
+  message->installAsQtMessageHandler();
+
+  Application::provide(script);
+  Application::provide(message);
+
+  return &Application::instance();
+}
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    qmlRegisterType<mqg::Core::Engine>("mqg.Core.Engine", 1,0, "Engine");
-    qmlRegisterType<mqg::Core::Environment>("mqg.Core.Engine", 1,0,
-                                            "Environment");
+    qRegisterMetaType<Message>("Message");
+
+    qmlRegisterSingletonType<Application>("mqg.Core.Application", 1,0,
+                                          "App", app_provider);
 
     QQmlApplicationEngine engine;
-    engine.load(QStringLiteral("qrc:/main.qml"));
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
 }
