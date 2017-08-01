6a5e2ea9660b41f9b0bce02ebc02729f91068c7e
korut94 <andreamnt94@gmail.com>
2017-08-01 15:19:45 +0200

    Exit with error status if a command inside reports an error The build fails if no any tests passed with success.

--- docker/linux/run_test.sh
+++ docker/linux/run_test.sh
@@ -2,7 +2,10 @@
 
 # MakeQtGame
 # Copyright (C) 2017 Andrea Mantovani
-# Run the tests and clean the build directory
+# Run the tests and clean the build directory if all of them are passed
+# succesfully.
+
+set -e
 
 qmake
 make check --silent
