19ffdf4e6c7fb0e565b00caf6409b48e41ea8d16
korut94 <andreamnt94@gmail.com>
2018-03-13 14:33:30 +0100

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
+#include "QtModules/qtguiapplication.h"
+#include "QtModules/qtscriptengine.h"
+
 #include <iostream>
 
 int main (int argc, char **argv) {
-    std::cout << "Hello world!" << std::endl;
+    using namespace mqg::Service;
+    using namespace mqg::module::Qt;
+
+    mqg::IApplication *app = new QtGuiApplication(argc, argv);
+    Script::IScriptService *script = new QtScriptEngine();
+
+    script->start();
+
+    app->exec(argc, argv);
+
+    script->stop();
+
+    delete app;
+    delete script;
+
     return 0;
 }
